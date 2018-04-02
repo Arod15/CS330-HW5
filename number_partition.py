7f6e02003b389af8bd57ae225f3566ef43000afa
Arod15 <Arod1198@gmail.com>
2018-04-02 19:02:54 -0400

    Update number_partition.py

--- number_partition.py
+++ number_partition.py
@@ -1,9 +1,11 @@
+set = []
+
 def set_table(lst):
 	'''Creates the table for the dynamic programming back tracking'''
 	max_sum = sum(lst)
-
+	global set
 	#Initializes matrix with all 'O'
-	M = [['O' for j in range(max_sum)] for i in range(len(lst))]
+	M = [['O' for j in range(max_sum + 1)] for i in range(len(lst))]
 	
 	for i in range(len(lst)):
 		#Base case of empty set
@@ -11,6 +13,16 @@ def set_table(lst):
 
 	for i in range(len(lst)):
 		#Sets the values of the matrix
+		M[i][lst[i]] = 'X'
+		print(str(i) + " is " + str(lst[i]))
+		if len(set) != 0:
+			for x in set:
+				# loop through all columns of previous row and make additions
+				M[i][lst[i] + x] = 'X'
+				print("Let's add " + str(lst[i] + x) + " is " + str(lst[i]))
+			set += lst[i]
+
+
 	print(M)
 
 	
